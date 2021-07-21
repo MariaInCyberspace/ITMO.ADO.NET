9db7b52aef3b1c8b03a467b42821e2ff1b4d3675
Maria Spirid <lan194@hotmail.com>
2021-07-21 22:53:42 +0300

    Ex. 4.2 of Lab 3 (stored procedure)

--- Lab3/DBCommand/DBCommand/Form1.Designer.cs
+++ Lab3/DBCommand/DBCommand/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace DBCommand
             this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sqlCommand2 = new System.Data.SqlClient.SqlCommand();
             this.SuspendLayout();
             // 
             // sqlConnection1
@@ -64,11 +66,26 @@ namespace DBCommand
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вызов процедуры";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sqlCommand2
+            // 
+            this.sqlCommand2.Connection = this.sqlConnection1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -83,6 +100,8 @@ namespace DBCommand
         private System.Data.SqlClient.SqlCommand sqlCommand1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Data.SqlClient.SqlCommand sqlCommand2;
     }
 }
 
--- Lab3/DBCommand/DBCommand/Form1.cs
+++ Lab3/DBCommand/DBCommand/Form1.cs
@@ -46,5 +46,35 @@ namespace DBCommand
             }
             richTextBox1.Text = sb.ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sqlCommand2.CommandType = CommandType.StoredProcedure;
+            sqlCommand2.CommandText = "Ten Most Expensive Products";
+            using (sqlCommand2.Connection)
+            {
+                sqlCommand2.Connection.Open();
+
+                using (SqlDataReader reader = sqlCommand2.ExecuteReader())
+                {
+                    bool MoreResults = false;
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                sb.Append(reader[i].ToString() + "\t");
+                            }
+                            sb.Append(Environment.NewLine);
+                        }
+                        MoreResults = reader.NextResult();
+                    }
+                    while (MoreResults);
+                }
+            }
+            richTextBox1.Text = sb.ToString();
+        }
     }
 }
