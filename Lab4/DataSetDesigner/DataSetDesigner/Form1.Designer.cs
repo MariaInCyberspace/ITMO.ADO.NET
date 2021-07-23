6e5507bd0f268d73fc54443612e1468708bf29db
Maria Spirid <lan194@hotmail.com>
2021-07-23 20:52:49 +0300

    Ex. 1 of Lab 4 (using DataSetDesigner to create DataSet)

---
+++ Lab4/DataSetDesigner/DataSetDesigner/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace DataSetDesigner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getCustomersButton = new System.Windows.Forms.Button();
+            this.customersListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // getCustomersButton
+            // 
+            this.getCustomersButton.Location = new System.Drawing.Point(24, 35);
+            this.getCustomersButton.Name = "getCustomersButton";
+            this.getCustomersButton.Size = new System.Drawing.Size(115, 44);
+            this.getCustomersButton.TabIndex = 0;
+            this.getCustomersButton.Text = "Get Customers";
+            this.getCustomersButton.UseVisualStyleBackColor = true;
+            this.getCustomersButton.Click += new System.EventHandler(this.getCustomersButton_Click);
+            // 
+            // customersListBox
+            // 
+            this.customersListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.customersListBox.FormattingEnabled = true;
+            this.customersListBox.ItemHeight = 16;
+            this.customersListBox.Location = new System.Drawing.Point(0, 126);
+            this.customersListBox.Name = "customersListBox";
+            this.customersListBox.Size = new System.Drawing.Size(800, 324);
+            this.customersListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customersListBox);
+            this.Controls.Add(this.getCustomersButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getCustomersButton;
+        private System.Windows.Forms.ListBox customersListBox;
+    }
+}
+
---
+++ Lab4/DataSetDesigner/DataSetDesigner/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSetDesigner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getCustomersButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet northwindDataSet = new NorthwindDataSet();
+            NorthwindDataSetTableAdapters.CustomersTableAdapter customersTableAdapter 
+                = new NorthwindDataSetTableAdapters.CustomersTableAdapter();
+
+            customersTableAdapter.Fill(northwindDataSet.Customers);
+
+            foreach (NorthwindDataSet.CustomersRow customer in northwindDataSet.Customers.Rows)
+            {
+                customersListBox.Items.Add(customer.CompanyName);
+            }
+        }
+    }
+}
