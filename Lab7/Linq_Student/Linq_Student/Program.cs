c04440c49515cfc3626a07f0b12ebabc62b93ffe
Maria Spirid <lan194@hotmail.com>
2021-07-29 02:22:25 +0300

    Ex. 1 of Lab 7

---
+++ Lab7/Linq_Student/Linq_Student/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Student> studentQuery = from student in Student.students
+                                                where student.Scores[2] > 90
+                                                select student;
+            foreach (Student student in studentQuery)
+            {
+                Console.WriteLine("{0}, {1}", student.Last, student.First);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Lab7/Linq_Student/Linq_Student/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Student
+{
+    public class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores;
+
+        public static List<Student> students = new List<Student>
+        {
+            new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+            new Student {First="Claire", Last="O’Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+        };
+    }
+}
