1d961bf5c1b0788eee3c3dd3c7ebe946f35d05e6
Maria Spirid <lan194@hotmail.com>
2021-07-30 04:31:07 +0300

    Started working on ex. 1 of Lab 9

---
+++ Lab9/CustomerManager/CustomerManager/CustomerViewer.cs
@@ -0,0 +1,91 @@
+﻿using CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CodeFirst.Model;
+
+namespace CustomerManager
+{
+    public partial class CustomerViewer : Form
+    {
+        SampleContext context = new SampleContext();
+        byte[] Ph;
+
+        public List<Order> Orders { get; private set; }
+
+        public CustomerViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Output()
+        {
+            if (this.customerRadioButton.Checked == true)
+                GridView.DataSource = context.Customers.ToList();
+            else if (this.orderRadioButton.Checked == true)
+                GridView.DataSource = context.Orders.ToList();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Customer customer = new Customer
+                {
+                    Name = this.textBoxName.Text,
+                    LastName = this.textBoxLastName.Text,
+                    Email = this.textBoxMail.Text,
+                    Age = Int32.Parse(this.textBoxAge.Text),
+                    Photo = Ph
+                };
+                context.Customers.Add(customer);
+                Orders = orderListBox.SelectedItems.OfType<Order>().ToList();
+                context.SaveChanges();
+                Output();
+                textBoxName.Text = String.Empty;
+                textBoxLastName.Text = String.Empty;
+                textBoxMail.Text = String.Empty;
+                textBoxAge.Text = String.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.ToString());
+            }
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog();
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                Image bm = new Bitmap(diag.OpenFile());
+
+                ImageConverter converter = new ImageConverter();
+                Ph = (byte[])converter.ConvertTo(bm, typeof(byte[]));
+            }
+        }
+
+        private void buttonOut_Click(object sender, EventArgs e)
+        {
+            Output();
+            var query = from b in context.Customers
+                        orderby b.Name
+                        select b;
+            customerList.DataSource = query.ToList();
+        }
+
+        private void CustomerViewer_Load(object sender, EventArgs e)
+        {
+            context.Orders.Add(new Order { ProductName = "Аудио", Quantity = 12, PurchaseDate = DateTime.Parse("12.01.2016") });
+            context.Orders.Add(new Order { ProductName = "Видео", Quantity = 22, PurchaseDate = DateTime.Parse("10.01.2016") });
+            context.SaveChanges();
+            orderListBox.DataSource = context.Orders.ToList();
+        }
+    }
+}
